9d24d7c51cf9e6e502c5e873751893313e8e0664
mabromuela88 <mabromuela.88@gmail.com>
2026-09-06 13:20:40 +0600

    Updated source file

--- script/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-const button = document.querySelector('.menu-btn');
-const links = document.querySelector('.nav-links');
-button.addEventListener('click', () => links.classList.toggle('open'));
